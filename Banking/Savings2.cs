526b8682abe9ebc33b2dfca248ac639ac6b51fd6
Matthew Luedtke <MLuedtke@tql.com>
2021-05-21 15:42:10 -0400

    update with Composition

---
+++ Banking/Savings2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking
+{
+    class Savings2
+    {
+        private Account account { get; set; }
+        public decimal InterestRate { get; set; } = 0.01m;
+
+        public Savings2()
+        {
+            account = new Account();
+        }
+        
+        public bool Deposit(decimal amount)
+        {
+            return account.Deposit(amount);
+        }
+        public bool Transfer(Account accountTo, decimal amount)
+        {
+            return account.Transfer(accountTo, amount);
+        }
+        public bool Withdraw(decimal amount)
+        {
+            return account.Withdraw(amount);
+        }
+
+        public decimal CalcInMonths(int months)
+        {
+            return account.Balance * (InterestRate / 12) * months;
+        }
+        public void PayInterest(int months)
+        {
+            Deposit(CalcInMonths(months));
+        }
+    }
+}
